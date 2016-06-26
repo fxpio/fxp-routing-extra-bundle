8dd4a806e705f3f0883086930651536fa4376427
François Pluchino <francois.pluchino@sonatra.com>
2016-06-26 19:17:39 +0200

    Add router extra service

---
+++ DependencyInjection/SonatraRoutingExtraExtension.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+ * This file is part of the Sonatra package.
+ *
+ * (c) François Pluchino <francois.pluchino@sonatra.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sonatra\Bundle\RoutingExtraBundle\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Loader;
+
+/**
+ * This is the class that loads and manages your bundle configuration.
+ *
+ * @author François Pluchino <francois.pluchino@sonatra.com>
+ */
+class SonatraRoutingExtraExtension extends Extension
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('routing_extra.xml');
+    }
+}
